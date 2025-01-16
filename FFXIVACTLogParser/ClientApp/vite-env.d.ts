06ed60e8827710e408ca1c69225efa849719d5f6
Bryce <brycec848@gmail.com>
2025-01-15 22:36:13 -0800

    typing changes to account for primevue

--- FFXIVACTLogParser/ClientApp/vite-env.d.ts
+++ FFXIVACTLogParser/ClientApp/vite-env.d.ts
@@ -5,3 +5,9 @@ declare module '*.vue' {
     const component: DefineComponent<{}, {}, any>;
     export default component;
 }
+
+declare module 'primevue/*' {
+    import { DefineComponent } from 'vue';
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
+}
